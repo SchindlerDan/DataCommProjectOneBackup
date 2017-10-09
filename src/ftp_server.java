17171316e7e1dbb04859e31cd7a09237d327d7bf
Brendan Cronan <32400374+brendan-cronan@users.noreply.github.com>
2017-10-09 18:17:07 -0400

    Testing a new branch

--- src/ftp_server.java
+++ src/ftp_server.java
@@ -1,99 +1,83 @@
 
-
-import java.io.*; 
+import java.io.*;
 import java.net.*;
 import java.util.*;
+
 //fixme
 class ftp_server {
-	public static final int LISTEN_PORT=6603;
-	public static final int COMMAND_PORT=6605;
+	public static final int LISTEN_PORT = 6603;
+	public static final int COMMAND_PORT = 6605;
 
+	public static void main(String argv[]) throws Exception {
 
-    public static void main(String argv[]) throws Exception {
-
-    	
 		String fromClient;
 		String clientCommand;
 		Socket dataSocket;
-	
+
 		ServerSocket welcomeSocket = new ServerSocket(LISTEN_PORT);
 		String frstln;
-	  
-		while(true){
-			Socket connectionSocket = welcomeSocket.accept();
-			DataOutputStream  outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
-			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-		
-			fromClient = inFromClient.readLine();
+		while (true) {
+			Socket connectionSocket = welcomeSocket.accept();
 			
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+			fromClient = inFromClient.readLine();
+
 			StringTokenizer tokens = new StringTokenizer(fromClient);
-		
+
 			frstln = tokens.nextToken();
 			int port = Integer.parseInt(frstln);
 			clientCommand = tokens.nextToken();
-			
-			if(clientCommand.equals("list:")){ 
+
+			if (clientCommand.equals("list:")) {
 				dataSocket = new Socket(connectionSocket.getInetAddress(), port);
-				DataOutputStream  dataOut = new DataOutputStream(new BufferedOutputStream(dataSocket.getOutputStream()));
-				//begin new code.
-				
+				DataOutputStream dataOut = new DataOutputStream(new BufferedOutputStream(dataSocket.getOutputStream()));
+				// begin new code.
+
 				File localStorage = new File("./");
 				File[] localFiles = localStorage.listFiles();
-				
-				
-				for (File f:localFiles)
+
+				for (File f : localFiles)
 					dataOut.writeBytes(f.getName());
 
+				// Resume given code.
+				dataSocket.close();
+				dataOut.close();
+				System.out.println("Data Socket closed");
 
-				//Resume given code.
-			dataSocket.close();    
-			dataOut.close();
-			System.out.println("Data Socket closed");
-			
 			}
-			//begin new code.
+			// begin new code.
 
-			
-			
-			
-			
-			
-			
-			//end new code. 
-			if(clientCommand.equals("retr:")){
+			// end new code.
+			if (clientCommand.equals("retr:")) {
 				// begin new code.
-				
+
 				dataSocket = new Socket(connectionSocket.getInetAddress(), port);
-				DataOutputStream  dataOut = new DataOutputStream(new BufferedOutputStream(dataSocket.getOutputStream()));
-							
-				
-				try{
-					String fileName=tokens.nextToken();
-					File f=new File(fileName);
-					
-					FileInputStream fileIn= new FileInputStream(f);
-					
-					byte[] fileBytes=new byte[(int) f.length()];
-					
-					
+				DataOutputStream dataOut = new DataOutputStream(new BufferedOutputStream(dataSocket.getOutputStream()));
+
+				try {
+					String fileName = tokens.nextToken();
+					File f = new File(fileName);
+
+					FileInputStream fileIn = new FileInputStream(f);
+
+					byte[] fileBytes = new byte[(int) f.length()];
+
 					fileIn.read(fileBytes);
 					dataOut.write(fileBytes);
-					
+
 					fileIn.close();
 					dataOut.close();
 					System.out.println("Data Socket closed");
 					System.out.println("Data Stream closed");
-					
-					
-				}catch(Exception e){
+
+				} catch (Exception e) {
 					e.printStackTrace(System.out);
 				}
-				
-				
-				
-				//end new code.
+
+				// end new code.
 			}
 		}
-    }
+	}
 }
